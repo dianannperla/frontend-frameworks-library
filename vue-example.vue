346aa17ff5945ddf88e372f457201a001ecc3ca6
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:25:17 +0100

    feat: initial commit for vue-example.vue

---
+++ vue-example.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Ví dụ về Vue.js</h1>
+    <p>Đây là một ví dụ đơn giản về cách sử dụng Vue.js.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueExample',
+};
+</script>
