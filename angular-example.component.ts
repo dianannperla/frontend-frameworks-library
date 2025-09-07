73956f466f1747aa285cde67df73b59b23fc283b
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:25:19 +0100

    feat: initial commit for angular-example.component.ts

---
+++ angular-example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-example',
+  template: `
+    <h1>Ví dụ về Angular</h1>
+    <p>Đây là một ví dụ đơn giản về cách sử dụng Angular.</p>
+  `,
+})
+export class AngularExampleComponent {}
