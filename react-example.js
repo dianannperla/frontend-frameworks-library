f5e1cb0a7bfec2503879b3f1201ad1b70c1f0257
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:26:13 +0100

    Update heading to be more descriptive

--- react-example.js
+++ react-example.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ReactExample = () => {
   return (
     <div>
-      <h1>Example of React</h1>
+      <h1>Simple React Example Component</h1>
       <p>Đây là một ví dụ đơn giản về cách sử dụng React.</p>
     </div>
   );
