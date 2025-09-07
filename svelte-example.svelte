b4cda6d96f21b2271b5cb8fa754b4b0553893882
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:25:30 +0100

    Update message to include English translation.

--- svelte-example.svelte
+++ svelte-example.svelte
@@ -1,5 +1,5 @@
 <script>
-  let message = 'Đây là một ví dụ đơn giản về cách sử dụng Svelte.';
+  let message = 'Đây là một ví dụ đơn giản về cách sử dụng Svelte. (This is a simple example of how to use Svelte.)';
 </script>
 
 <h1>Ví dụ về Svelte</h1>
