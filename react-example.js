0ed9e9c156a3be7b6655984ce3f466f90f2886b5
dianannperla <waafirmelyza@hotmail.com>
2025-09-07 20:25:16 +0100

    feat: initial commit for react-example.js

---
+++ react-example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReactExample = () => {
+  return (
+    <div>
+      <h1>Ví dụ về React</h1>
+      <p>Đây là một ví dụ đơn giản về cách sử dụng React.</p>
+    </div>
+  );
+};
+
+export default ReactExample;
